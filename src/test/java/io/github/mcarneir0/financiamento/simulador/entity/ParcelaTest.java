b0ba3ed1005ecc784ab50315d1031d9572c5ec7c
mcarneir0 <matheusfeitosa@outlook.com>
2026-05-22 16:33:01 -0300

    Adicionado testes unitários de Parcela

---
+++ src/test/java/io/github/mcarneir0/financiamento/simulador/entity/ParcelaTest.java
@@ -0,0 +1,38 @@
+package io.github.mcarneir0.financiamento.simulador.entity;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@QuarkusTest
+public class ParcelaTest {
+
+    @Test
+    void testParcela() {
+        Parcela parcela = new Parcela();
+        parcela.setMes(LocalDate.now());
+        parcela.setSaldoFinal(BigDecimal.valueOf(150));
+        parcela.setSaldoInicial(BigDecimal.valueOf(100));
+        parcela.setValorJuros(BigDecimal.valueOf(50));
+        assert parcela.getMes() != null;
+        assert parcela.getSaldoInicial() != null;
+        assert parcela.getValorJuros() != null;
+        assert parcela.getSaldoFinal() != null;
+    }
+
+    @Test
+    void testParcelaConstrutor() {
+        Parcela parcela = new Parcela(
+                LocalDate.now(),
+                BigDecimal.valueOf(100),
+                BigDecimal.valueOf(50),
+                BigDecimal.valueOf(150)
+        );
+        assert parcela.getMes() != null;
+        assert parcela.getSaldoInicial() != null;
+        assert parcela.getValorJuros() != null;
+        assert parcela.getSaldoFinal() != null;
+    }
+}
