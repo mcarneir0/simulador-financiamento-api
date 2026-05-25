f42800e0412577d3d2843bdd32850e71ff591168
mcarneir0 <matheusfeitosa@outlook.com>
2026-05-25 00:13:25 -0300

    Adicionado teste de integração

---
+++ src/test/java/io/github/mcarneir0/financiamento/simulador/resource/SimulacaoResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.mcarneir0.financiamento.simulador.resource;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class SimulacaoResourceIT extends SimulacaoResourceTest {
+    // Executa os mesmos testes do SimulacaoResourceTest
+}
